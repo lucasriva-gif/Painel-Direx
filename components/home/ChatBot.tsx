a237660f9085282a3b1f29ed6c5eec07f3c183a5
Lucas Riva <lucas.riva@magazineluiza.intranet>
2026-03-19 10:42:06 -0300

    mudança de modelo para o deepseek

--- components/home/ChatBot.tsx
+++ components/home/ChatBot.tsx
@@ -47,7 +47,7 @@ const ChatBot: React.FC = () => {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-            model: 'deepseek-coder-v2:16b',
+            model: 'llama3.1',
             messages: [...updatedMessages],
             stream: true,
         }),
